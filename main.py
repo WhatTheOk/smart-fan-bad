fbf26947d10978b7b5bcf8944ed37e6c045bbd37
WhatTheOk <98926807+WhatTheOk@users.noreply.github.com>
2023-02-18 15:55:50 -0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+def adjustMode():
+    if fanData[0] == 0:
+        adjustSpeed(0)
+    if fanData[0] == 1:
+        adjustSpeed(fanData[1])
+    if fanData[0] == 2:
+        timeFan()
+    if fanData[0] == 3:
+        tempFan()
+
+def adjustSpeed(speed: number):
+    pins.analog_write_pin(AnalogPin.P4, speed * 1023 / 9)
+    pins.digital_write_pin(DigitalPin.P8, 0)
+    pins.digital_write_pin(DigitalPin.P9, 0)
+    pins.digital_write_pin(DigitalPin.P10, 0)
+    if speed > 6:
+        pins.digital_write_pin(DigitalPin.P8, 1)
+    elif speed > 3:
+        pins.digital_write_pin(DigitalPin.P9, 1)
+    elif speed > 0:
+        pins.digital_write_pin(DigitalPin.P10, 1)
+
+def timeFan():
+    global hour, minute, terminate
+    adjustSpeed(fanData[1])
+    ds.set_hour(0)
+    ds.set_minute(0)
+    hour = int(fanData[2] / 100)
+    minute = fanData[2] % 100
+    terminate = False
+    while fanData[0] == 2 and terminate == False:
+        if hour == ds.get_hour() and minute == ds.get_minute():
+            terminate = True
+        basic.pause(1000)
+    fanData[0] = 0
+
 def tempFan():
     global minTemp, maxTemp, temper
     minTemp = fanData[3]
@@ -11,38 +47,8 @@ def tempFan():
         else:
             adjustSpeed((temper - minTemp) / (maxTemp - minTemp) * 9)
         basic.pause(2000)
-def adjustMode():
-    if fanData[0] == 0:
-        adjustSpeed(0)
-    if fanData[0] == 1:
-        adjustSpeed(fanData[1])
-    if fanData[0] == 2:
-        timeFan()
-    if fanData[0] == 3:
-        tempFan()
-def timeFan():
-    global hour, minute
-    ds.set_hour(0)
-    ds.set_minute(0)
-    hour = int(fanData[2] / 100)
-    minute = fanData[2] % 100
-    terminate = False
-    while fanData[0] == 2 and terminate == False:
-        if(hour == ds.get_hour() and minute == ds.get_minute()):
-            terminate = True
-        basic.pause(1000)
-    fanData[0] = 0
-def adjustSpeed(speed: number):
-    pins.analog_write_pin(AnalogPin.P4, speed * 1023 / 9)
-    pins.digital_write_pin(DigitalPin.P8, 0)
-    pins.digital_write_pin(DigitalPin.P9, 0)
-    pins.digital_write_pin(DigitalPin.P10, 0)
-    if speed > 6:
-        pins.digital_write_pin(DigitalPin.P8, 1)
-    elif speed > 3:
-        pins.digital_write_pin(DigitalPin.P9, 1)
-    elif speed > 0:
-        pins.digital_write_pin(DigitalPin.P10, 1)
+
+terminate = False
 minute = 0
 hour = 0
 ds: DS1302.DS1302RTC = None
@@ -55,7 +61,7 @@ maxTemp = 0
 temper = 0
 led.enable(True)
 makerbit.connect_ir_receiver(DigitalPin.P0, IrProtocol.KEYESTUDIO)
-fanData = [2, 5, 230, 20, 30]
+fanData = [0, 5, 230, 20, 30]
 ds = DS1302.create(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15)
 dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P1, True, False, False)
 adjustMode()
--- main.ts
+++ main.ts
@@ -1,46 +1,23 @@
-function tempFan () {
-    minTemp = fanData[3]
-    maxTemp = fanData[4]
-    while (fanData[0] == 1) {
-        temper = randint(21, 29)
-        if (temper < minTemp) {
-            adjustSpeed(0)
-        } else if (temper > maxTemp) {
-            adjustSpeed(9)
-        } else {
-            adjustSpeed((temper - minTemp) / (maxTemp - minTemp) * 9)
-        }
-        basic.pause(2000)
-    }
-}
-function adjustMode () {
+function adjustMode() {
     if (fanData[0] == 0) {
         adjustSpeed(0)
     }
+    
     if (fanData[0] == 1) {
         adjustSpeed(fanData[1])
     }
+    
     if (fanData[0] == 2) {
         timeFan()
     }
+    
     if (fanData[0] == 3) {
         tempFan()
     }
+    
 }
-function timeFan () {
-    ds.setHour(0)
-    ds.setMinute(0)
-    hour = Math.trunc(fanData[2] / 100)
-    minute = fanData[2] % 100
-    while (fanData[0] == 2 && terminate == false) {
-        if (hour == ds.getHour() && minute == ds.getMinute()) {
-            terminate = true
-        }
-        basic.pause(1000)
-    }
-    fanData[0] = 0
-}
-function adjustSpeed (speed: number) {
+
+function adjustSpeed(speed: number) {
     pins.analogWritePin(AnalogPin.P4, speed * 1023 / 9)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P9, 0)
@@ -52,12 +29,50 @@ function adjustSpeed (speed: number) {
     } else if (speed > 0) {
         pins.digitalWritePin(DigitalPin.P10, 1)
     }
+    
 }
+
+function timeFan() {
+    
+    adjustSpeed(fanData[1])
+    ds.setHour(0)
+    ds.setMinute(0)
+    hour = Math.trunc(fanData[2] / 100)
+    minute = fanData[2] % 100
+    terminate = false
+    while (fanData[0] == 2 && terminate == false) {
+        if (hour == ds.getHour() && minute == ds.getMinute()) {
+            terminate = true
+        }
+        
+        basic.pause(1000)
+    }
+    fanData[0] = 0
+}
+
+function tempFan() {
+    
+    minTemp = fanData[3]
+    maxTemp = fanData[4]
+    while (fanData[0] == 1) {
+        temper = randint(21, 29)
+        if (temper < minTemp) {
+            adjustSpeed(0)
+        } else if (temper > maxTemp) {
+            adjustSpeed(9)
+        } else {
+            adjustSpeed((temper - minTemp) / (maxTemp - minTemp) * 9)
+        }
+        
+        basic.pause(2000)
+    }
+}
+
 let terminate = false
 let minute = 0
 let hour = 0
-let ds: DS1302.DS1302RTC = null
-let fanData: number[] = []
+let ds : DS1302.DS1302RTC = null
+let fanData : number[] = []
 let temper = 0
 let maxTemp = 0
 let minTemp = 0
@@ -66,19 +81,7 @@ maxTemp = 0
 temper = 0
 led.enable(true)
 makerbit.connectIrReceiver(DigitalPin.P0, IrProtocol.Keyestudio)
-fanData = [
-2,
-5,
-230,
-20,
-30
-]
+fanData = [0, 5, 230, 20, 30]
 ds = DS1302.create(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15)
-dht11_dht22.queryData(
-DHTtype.DHT11,
-DigitalPin.P1,
-true,
-false,
-false
-)
+dht11_dht22.queryData(DHTtype.DHT11, DigitalPin.P1, true, false, false)
 adjustMode()
