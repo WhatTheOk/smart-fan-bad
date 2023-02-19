8b21a867959142ff0d07ab7238157e6c3b32f168
WhatTheOk <98926807+WhatTheOk@users.noreply.github.com>
2023-02-18 16:06:19 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,48 @@
-function adjustMode() {
+function tempFan () {
+    minTemp = fanData[3]
+    maxTemp = fanData[4]
+    while (fanData[0] == 1) {
+        temper = randint(21, 29)
+        if (temper <= minTemp) {
+            adjustSpeed(0)
+        } else if (temper >= maxTemp) {
+            adjustSpeed(9)
+        } else {
+            adjustSpeed((temper - minTemp) / (maxTemp - minTemp) * 9)
+        }
+        basic.pause(2000)
+    }
+}
+function adjustMode () {
     if (fanData[0] == 0) {
         adjustSpeed(0)
     }
-    
     if (fanData[0] == 1) {
         adjustSpeed(fanData[1])
     }
-    
     if (fanData[0] == 2) {
         timeFan()
     }
-    
     if (fanData[0] == 3) {
         tempFan()
     }
-    
 }
-
-function adjustSpeed(speed: number) {
+function timeFan () {
+    adjustSpeed(fanData[1])
+    ds.setHour(0)
+    ds.setMinute(0)
+    hour = Math.trunc(fanData[2] / 100)
+    minute = fanData[2] % 100
+    terminate = false
+    while (fanData[0] == 2 && terminate == false) {
+        if (hour == ds.getHour() && minute == ds.getMinute()) {
+            terminate = true
+        }
+        basic.pause(1000)
+    }
+    fanData[0] = 0
+}
+function adjustSpeed (speed: number) {
     pins.analogWritePin(AnalogPin.P4, speed * 1023 / 9)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P9, 0)
@@ -29,50 +54,12 @@ function adjustSpeed(speed: number) {
     } else if (speed > 0) {
         pins.digitalWritePin(DigitalPin.P10, 1)
     }
-    
 }
-
-function timeFan() {
-    
-    adjustSpeed(fanData[1])
-    ds.setHour(0)
-    ds.setMinute(0)
-    hour = Math.trunc(fanData[2] / 100)
-    minute = fanData[2] % 100
-    terminate = false
-    while (fanData[0] == 2 && terminate == false) {
-        if (hour == ds.getHour() && minute == ds.getMinute()) {
-            terminate = true
-        }
-        
-        basic.pause(1000)
-    }
-    fanData[0] = 0
-}
-
-function tempFan() {
-    
-    minTemp = fanData[3]
-    maxTemp = fanData[4]
-    while (fanData[0] == 1) {
-        temper = randint(21, 29)
-        if (temper < minTemp) {
-            adjustSpeed(0)
-        } else if (temper > maxTemp) {
-            adjustSpeed(9)
-        } else {
-            adjustSpeed((temper - minTemp) / (maxTemp - minTemp) * 9)
-        }
-        
-        basic.pause(2000)
-    }
-}
-
 let terminate = false
 let minute = 0
 let hour = 0
-let ds : DS1302.DS1302RTC = null
-let fanData : number[] = []
+let ds: DS1302.DS1302RTC = null
+let fanData: number[] = []
 let temper = 0
 let maxTemp = 0
 let minTemp = 0
@@ -81,7 +68,19 @@ maxTemp = 0
 temper = 0
 led.enable(true)
 makerbit.connectIrReceiver(DigitalPin.P0, IrProtocol.Keyestudio)
-fanData = [0, 5, 230, 20, 30]
+fanData = [
+0,
+5,
+230,
+20,
+30
+]
 ds = DS1302.create(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15)
-dht11_dht22.queryData(DHTtype.DHT11, DigitalPin.P1, true, false, false)
+dht11_dht22.queryData(
+DHTtype.DHT11,
+DigitalPin.P1,
+true,
+false,
+false
+)
 adjustMode()
