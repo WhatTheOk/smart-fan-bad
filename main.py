125fd3023ac1cfba407c16b11c221e3f1f24061d
WhatTheOk <98926807+WhatTheOk@users.noreply.github.com>
2023-02-23 09:48:53 -0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def tempFan():
     global minTemp, maxTemp, temper
     minTemp = fanData[3]
     maxTemp = fanData[4]
-    while fanData[0] == 1:
+    while fanData[0] == 3:
         temper = randint(21, 29)
         if temper <= minTemp:
             adjustSpeed(0)
@@ -14,6 +14,8 @@ def tempFan():
 
 def on_button_pressed_a():
     fanData[1] = (int(fanData[1] / 3) * 3 + 3) % 12
+    if fanData[0] != 0:
+        adjustSpeed(fanData[1])
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def adjustMode():
@@ -34,6 +36,8 @@ def adjustMode():
 
 def on_button_pressed_b():
     fanData[0] = (fanData[0] + 1) % 4
+    basic.show_number(fanData[0])
+    adjustMode()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def timeFan():
@@ -48,6 +52,7 @@ def timeFan():
         if ds.get_hour() == hour and ds.get_minute() == minute:
             fanData[0] = 0
         basic.pause(1000)
+
 def adjustSpeed(speed: number):
     pins.analog_write_pin(AnalogPin.P4, 348 + speed * 75)
     if speed == 0:
@@ -59,9 +64,9 @@ maxTemp = 0
 minTemp = 0
 ds: DS1302.DS1302RTC = None
 fanData: List[number] = []
-led.enable(False)
+led.enable(True)
 makerbit.connect_ir_receiver(DigitalPin.P0, IrProtocol.KEYESTUDIO)
-fanData = [0, 5, 230, 20, 30]
+fanData = [3, 5, 230, 20, 30]
 ds = DS1302.create(DigitalPin.P13, DigitalPin.P14, DigitalPin.P15)
 dht11_dht22.query_data(DHTtype.DHT11, DigitalPin.P1, True, False, False)
 adjustMode()
